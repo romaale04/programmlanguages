5c97973f7ffa007ebd3ecb8ffe6c80ef7feed1dd
Romanov Aleksej <roman-off13@mail.ru>
2022-10-17 19:19:55 +0300

    Задача3

---
+++ Знакомство с языками программирования/Домашнее задание 4/Задача3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 7, 8
+
+int[] CreateArray() 
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+      array [i] = new Random().Next(0,21);
+      
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+       
+        Console.WriteLine($"{array[i]}");
+    }
+} 
+
+int [] arr = CreateArray();
+PrintArray(arr);
