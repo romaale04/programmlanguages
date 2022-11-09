f1935da494aecfe721cb15ea9d7afa4da1bd4c9d
Romanov Aleksej <roman-off13@mail.ru>
2022-11-09 19:24:10 +0300

    Задача1

---
+++ GeekBrains/Знакомство с языками программирования/Домашнее задание/Домашнее задание 7/Задача1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+double[,] CreateArray(int lenRows, int lenColumns)
+{
+    double[,] array = new double[lenRows, lenColumns];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-10, 10)) / 1.1;
+        }
+        
+    } 
+    return array;
+}
+
+void PrintArray(double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+        
+    } 
+    
+}
+
+double[,] array = CreateArray(3, 4);
+PrintArray(array);
---
+++ GeekBrains/Знакомство с языками программирования/Лекции/Лекция 5 как не нужно писать код/Program.cs
@@ -0,0 +1,8 @@
+﻿// // Вид3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
