d007c16237b593fa24fb9f731dd4c737a012d811
Romanov Aleksej <roman-off13@mail.ru>
2022-11-19 16:23:04 +0300

    Задача2

---
+++ GeekBrains/Знакомство с языками программирования/Домашнее задание/Итоговый проект/Задача2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintNumbers(int m, int n, int sum)
+{
+    if (m > n)
+    {
+       Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");  
+       return;
+    }
+
+    sum = sum + (m);
+    PrintNumbers(m, n, sum);
+}
+
+int m = Prompt("Введите число m: ");
+int n = Prompt("Введите число n: ");
