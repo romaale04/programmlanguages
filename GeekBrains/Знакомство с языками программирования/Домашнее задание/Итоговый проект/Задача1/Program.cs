3f3697a0e640e77887d4493a8971543c062a2481
Romanov Aleksej <roman-off13@mail.ru>
2022-11-19 16:23:43 +0300

    Задача1

---
+++ GeekBrains/Знакомство с языками программирования/Домашнее задание/Итоговый проект/Задача1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintNumbers(int m, int n)
+{
+    if (m < n)
+    {
+        return ;
+    }
+
+    System.Console.Write(m + " ");
+    PrintNumbers(m + 1, n);
+}
+
+int NatNum(int num)
+{
+    if (num > m && num < n)
+    {
+        return NatNum (num % 2 == 0);
+    }
+return 0;
+    
+
+}
+
+int m = Prompt("Введите число m: ");
+int n = Prompt("Введите число n: ");
+PrintNumbers(NatNum);
